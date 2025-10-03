2f2469c7da3baae262df52af830ccb1605a4a4a2
yooneerum <yooneerum@gmail.com>
2025-10-03 21:01:05 +0900

    helloapp files update

---
+++ helloapp/src/Component_class.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Component_class extends Component {
+  render() {
+    return (
+      <div>Component_class</div>
+    )
+  }
+}
---
+++ helloapp/src/Component_function.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Component_function() {
+  return (
+    <div>Component_function</div>
+  )
+}
