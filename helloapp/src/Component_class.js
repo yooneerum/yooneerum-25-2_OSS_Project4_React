0bff542e06d4ef8ebc980f21a5bd8f89a67260bc
윤이름 <65767346+yooneerum@users.noreply.github.com>
2025-10-03 22:28:41 +0900

    Delete helloapp directory

--- helloapp/src/Component_class.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Component_class extends Component {
-  render() {
-    return (
-      <div>Component_class</div>
-    )
-  }
-}
--- helloapp/src/Component_function.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Component_function() {
-  return (
-    <div>Component_function</div>
-  )
-}
